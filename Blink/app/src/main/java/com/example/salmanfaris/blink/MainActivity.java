0207d128843869a4117ed550771c41fa0d047885
salmanfarisvp <farissalmannbr@gmail.com>
2017-09-26 17:09:22 +0530

    raspberry pi blink

---
+++ Blink/app/src/main/java/com/example/salmanfaris/blink/MainActivity.java
@@ -0,0 +1,100 @@
+package com.example.salmanfaris.blink;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+
+/**
+ * Skeleton of an Android Things activity.
+ *
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ *
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ *
+ */
+public class MainActivity extends Activity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int INTERVAL_BETWEEN_BLINKS_MS = 500;
+    private static final String GPIO_PIN_NAME = "BCM13"; // Physical Pin #33 on Raspberry Pi3
+
+
+    private Handler mHandler = new Handler();
+
+    private Gpio mLedGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        // Step 1. Create GPIO connection.
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            mLedGpio = service.openGpio(GPIO_PIN_NAME);
+            // Step 2. Configure as an output.
+            mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+
+            // Step 4. Repeat using a handler.
+            mHandler.post(mBlinkRunnable);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Step 4. Remove handler events on close.
+        mHandler.removeCallbacks(mBlinkRunnable);
+
+        // Step 5. Close the resource.
+        if (mLedGpio != null) {
+            try {
+                mLedGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    }
+
+    private Runnable mBlinkRunnable = new Runnable() {
+        @Override
+        public void run() {
+            // Exit if the GPIO is already closed
+            if (mLedGpio == null) {
+                return;
+            }
+
+            try {
+                // Step 3. Toggle the LED state
+                mLedGpio.setValue(!mLedGpio.getValue());
+
+                // Step 4. Schedule another event after delay.
+                mHandler.postDelayed(mBlinkRunnable, INTERVAL_BETWEEN_BLINKS_MS);
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    };
+}
